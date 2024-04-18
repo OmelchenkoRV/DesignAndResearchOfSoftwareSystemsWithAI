e84f8a30eb1847f8d4da381d2b6aea61e46889d4
OmelchenkoRV <omelchenkorv@gmail.com>
2024-04-18 23:07:38 +0300

    rebuild the neural network

--- lab4.py
+++ lab4.py
@@ -14,47 +14,49 @@ cifar10 = tf.keras.datasets.cifar10
 
 print("Train samples:", x_train.shape, y_train.shape)
 print("Test samples:", x_test.shape, y_test.shape)
-NUM_CLASSES = 10
-cifar10_classes = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
 
-x_train2 = (x_train / 255) - 0.5
-x_test2 = (x_test / 255) - 0.5
-y_train2 = keras.utils.to_categorical(y_train, NUM_CLASSES)
-y_test2 = keras.utils.to_categorical(y_test, NUM_CLASSES)
+cifar10_classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
+
+y_train = keras.utils.to_categorical(y_train)
+y_test = keras.utils.to_categorical(y_test)
 
 model = Sequential([
-    layers.Conv2D(filters=96, kernel_size=(11, 11), activation='relu', input_shape=(32, 32, 3)),
-    layers.BatchNormalization(),
-    layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
-
-    layers.Conv2D(filters=256, kernel_size=(5, 5), activation='relu', padding='same'),
-    layers.BatchNormalization(),
-    layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
-
-    layers.Conv2D(filters=384, kernel_size=(3, 3), activation='relu', padding='same'),
-    layers.BatchNormalization(),
-
-    layers.Conv2D(filters=384, kernel_size=(3, 3), activation='relu', padding='same'),
-    layers.BatchNormalization(),
-
-    layers.Conv2D(filters=256, kernel_size=(5, 5), activation='relu', padding='same'),
-    layers.BatchNormalization(),
-    layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
-
-    layers.Flatten(),
-    layers.Dense(4096, activation='relu'),
+    # First convolutional layer
+    layers.Conv2D(filters=32, kernel_size=(4, 4), input_shape=(32, 32, 3), activation='relu'),
+    layers.Dropout(0.1),
+    # Second convolutional layer
+    layers.Conv2D(filters=32, kernel_size=(4, 4), activation='relu'),
+    layers.MaxPool2D(pool_size=(2, 2)),
+    layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'),
     layers.Dropout(0.2),
-    layers.Dense(4096, activation='relu'),
+
+    layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'),
+    layers.MaxPool2D(pool_size=(2, 2)),
+    # Pooling
+    layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same'),
+    layers.Dropout(0.2),
+     # Dropout
+    layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same'),
+    layers.MaxPool2D(pool_size=(2, 2)),
+    layers.Flatten(),
+    layers.Dropout(0.2),
+
+    layers.Dense(512, activation='relu'),
+    layers.Dropout(0.2),
+    layers.Dense(256, activation='relu'),
     layers.Dropout(0.2),
     layers.Dense(10, activation='softmax')
 ])
 
-model.compile(loss=SparseCategoricalCrossentropy(from_logits=False),
+model.compile(loss='categorical_crossentropy',
               optimizer=Adam(learning_rate=0.001),
               metrics=['accuracy'])
 model.summary()
-history = model.fit(x_train, y_train, epochs=1, validation_data=(x_test, y_test))
-y_pred_test = model.predict(x_test2)
+history = model.fit(x_train, y_train, epochs=20, batch_size=512, validation_data=(x_test, y_test))
+y_pred_test = model.predict(x_test)
 y_pred_test_classes = np.argmax(y_pred_test, axis=1)
 y_pred_test_max_probas = np.max(y_pred_test, axis=1)
 
@@ -70,7 +72,7 @@ for i in range(cols):
         ax.imshow(x_test[random_index, :])
         pred_label = cifar10_classes[y_pred_test_classes[random_index]]
         pred_proba = y_pred_test_max_probas[random_index]
-        true_label = cifar10_classes[y_test[random_index, 0]]
+        true_label = cifar10_classes[y_pred_test_classes[random_index]]
         ax.set_title("pred: {}\nscore: {:.3}\ntrue: {}".format(
             pred_label, pred_proba, true_label
         ))
